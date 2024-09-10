5acabc87567503a87c986c4cb4c28f1b64ffccb2
Harshita Devreddi <harshitadevreddi@gmail.com>
2024-09-10 19:07:05 +0530

    Add files via upload

---
+++ arr_missing_num(2).java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MissingNum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        int totalSum = n * (n + 1) / 2;
+        System.out.println("Enter " + (n - 1) + " integers from the range 1 to " + n + ":");
+        int actualSum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            actualSum += scanner.nextInt();
+        }
+
+        int missingNum = totalSum - actualSum;
+        System.out.println("The missing number is: " + missingNum);
+
+        scanner.close();
+    }
+}
